7b8b5b62c75a787abd0405c36ae99dfefde12e50
prashant1682 <prashant009@gmail.com>
2020-12-30 12:39:41 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function noteKeeper(noteItem) {
+  return (
+    <div>
+      <Note
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteKeeper)};
       <Footer />
     </div>
   );
